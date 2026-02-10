3ab4701c950473ef9593441690c916c0f4c767d5
glakshmisneha28 <vtu26082@veltech.edu.in>
2026-02-10 13:59:02 +0530

    Create FieldInjectionApplication.java

---
+++ WEEK-4/SESSION-17/TASK-2/springboot-field-injection/src/main/java/com/example/fieldinjection/FieldInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.fieldinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FieldInjectionApplication.class, args);
+    }
+
+}
