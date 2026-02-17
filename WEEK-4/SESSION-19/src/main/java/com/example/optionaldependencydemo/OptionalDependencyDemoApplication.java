1556ead86020f506be740272cf192fa4546c5312
glakshmisneha28 <vtu26082@veltech.edu.in>
2026-02-17 14:36:59 +0530

    Create OptionalDependencyDemoApplication.java

---
+++ WEEK-4/SESSION-19/src/main/java/com/example/optionaldependencydemo/OptionalDependencyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.optionaldependencydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionalDependencyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptionalDependencyDemoApplication.class, args);
+    }
+}
